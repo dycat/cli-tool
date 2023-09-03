5750c62cceaaf892d42ca64256818eb34229baef
dycat <8856528+dycat@users.noreply.github.com>
2023-09-03 15:34:45 +0800

    feat: clean output

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,2 +1,24 @@
 #!/usr/bin/env node
-console.log('hello tool');
+const arg = require('arg');
+
+try {
+    const args = arg({
+        '--start': Boolean,
+        '--build': Boolean,
+    })
+    
+    if (args['--start']) {
+        console.log('start the app');
+    }
+    
+} catch (e) {
+    console.log(e.message);
+    console.log();
+    usage();
+}
+
+function usage() {
+    console.log(`tool [CMD]
+    --start\tStarts the app
+    --build\tBuilds the app`);
+}
